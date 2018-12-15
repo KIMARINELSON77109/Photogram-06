db195914c8596793007f920940206370124f8b87
Omar Christie <omar.christie@mymona.uwi.edu>
2018-12-15 13:04:06 +0000

    runtime.txt

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app, db, filefolder, token_key
 from flask import render_template, request, redirect, url_for, flash, jsonify, g, session
-from forms import LoginForm, RegistrationForm, newPostForm
+from .forms import LoginForm, RegistrationForm, newPostForm
 from models import Users, Posts, Likes, Follows
 import os
 import jwt
